47fb554f7487adba084f2bf8eb709841edfda1db
Tomash Gombosh <blackneco@meta.ua>
2018-03-21 18:22:17 +0200

    Starting project
    Starting project for trainings

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePage {
+
+    private By homePageUserName = By.xpath("//table//tr[@class='heading3']");
+
+    public HomePage(WebDriver driver){
+        super(driver);
+    }
+
+    //Get the User name from Home Page
+    public String getHomePageDashboardUserName(){
+        return getText(homePageUserName);
+    }
+}
---
+++ src/test/java/tests/LoginTests.java
@@ -0,0 +1,38 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class LoginTests extends BaseTest {
+
+    private LoginPage objLogin;
+    private HomePage objHomePage;
+
+    /**
+     * This test case will login in http://demo.guru99.com/V4/
+     * Verify login page title as guru99 bank
+     * Login to application
+     * Verify the home page using Dashboard message
+     */
+    @Test(description = "Test Case 1 - User can login successfully")
+    public void userCanLoginSuccessfully(){
+        //Create Login Page object
+        objLogin = new LoginPage(driver);
+        //Verify login page title
+        String loginPageTitle = objLogin.getLoginTitle();
+        Assert.assertTrue(loginPageTitle.toLowerCase().contains("guru99 bank"));
+        //login to application
+        objLogin.loginToGuru99("mgr123", "mgr!23");
+        // go the next page
+        objHomePage = new HomePage(driver);
+        //Verify home page
+        Assert.assertTrue(objHomePage.getHomePageDashboardUserName().toLowerCase().contains("manger id : mgr123"));
+    }
+    @Test(description = "Test Case 2 - User can logout")
+    public void userCanLogout(){
+        Assert.assertTrue(false);
+    }
+
+}
