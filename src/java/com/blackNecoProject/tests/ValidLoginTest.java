90e433fd0bb4d2f1d414a71f37a6ba9676daedcd
KristianGombosh <gomboshkr@gmail.com>
2018-04-07 11:29:47 +0300

    New Test case(User login is valid)

---
+++ src/java/com/blackNecoProject/tests/ValidLoginTest.java
@@ -0,0 +1,22 @@
+package com.blackNecoProject.tests;
+
+import com.blackNecoProject.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ValidLoginTest extends BaseTest{
+        private LoginPage loginpage;
+
+
+        @Test(description = "Test Case 1 - User can't login successful")
+        public void userCantLoginSuccess (){
+            loginpage = new LoginPage(driver);
+            String loginPageTitle = loginpage.getLoginTitle();
+            Assert.assertTrue(loginPageTitle.toLowerCase().contains("guru99 bank"));
+            loginpage.loginToGuru99("mgr123", "mgr23");
+            loginpage.alere();
+            Assert.assertTrue(loginPageTitle.toLowerCase().contains("guru99 bank"));
+        }
+    }
+
+
